76b47cd98ee9e052ca0e4e39cdbe86b8dfd25c29
DenisEgorov200 <egorov.denis.mem@gmail.com>
2023-09-05 17:34:54 +0300

    feat(header): add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { Header } from './components/layout/Header.jsx';
+
 export const App = () => {
   return (
     <div>
+      <Header />
       <h1>Hello world</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      orange: '#F86600',
+      gray: '#232526',
+    },
+    fontFamily: {
+      passion: ['Passion One', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
