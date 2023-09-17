83f4353efe27f0cbd689d4775b96f0f22252359d
DenisEgorov200 <egorov.denis.mem@gmail.com>
2023-09-17 14:37:53 +0300

    feat(api): add start date in last week

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,9 +6,12 @@ const BASE_URL = `https://api.nasa.gov/neo/rest/v1/feed?`;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const fetchDataFromApi = async (params) => {
+  const date = new Date(params);
+  date.setDate(date.getDate() - 7);
+
   try {
     return await ky
-      .get(`${BASE_URL}start_date=${params}end_date=${params}&api_key=${API_KEY}`)
+      .get(`${BASE_URL}start_date=${formatDate(date)}end_date=${params}&api_key=${API_KEY}`)
       .json();
   } catch (error) {
     console.log(error.message);
